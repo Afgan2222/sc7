8cb15509dda21f0b77323480f3576da349bca593
Afgan2222 <108980042+Afgan2222@users.noreply.github.com>
2022-09-25 18:16:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,8 +9,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
+global.ownerNumber = ['6282125968810','6282125968810','0']
+global.ownerName = 'GannReall💀💀'
 global.packname = 'Ichigo Kurosaki'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
